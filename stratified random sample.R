b32506cfa89a6486abfb8da82dda252f0d1c4c2b
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-03-19 00:13:20 -0400

    Update stratified random sample.R

--- stratified random sample.R
+++ stratified random sample.R
@@ -2,7 +2,21 @@ data=read.csv("C:/Users/Phoebe/Desktop/Environmental statistics/hw.csv")
 N=100
 x=sample(1:100,20)
 x
+[1] 80 28  5  2 53 51 38 57 48 60 56 75 66 98  8 90 52 73 18 32
 stem(x,scale=2)
+ The decimal point is 1 digit(s) to the right of the |
+
+  0 | 258
+  1 | 8
+  2 | 8
+  3 | 28
+  4 | 8
+  5 | 12367
+  6 | 06
+  7 | 35
+  8 | 0
+  9 | 08
+
 x.1=c(2,5,32)
 x.2=c(8,18,28,38)
 x.3=c(51,52,53,56,66,73,75)
