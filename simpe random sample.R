2f5a9dacdbaace2da69f54d090dbf3ed4eab20f6
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-03-19 00:09:44 -0400

    Add files via upload

---
+++ simpe random sample.R
@@ -0,0 +1,14 @@
+data=read.csv("C:/Users/Phoebe/Desktop/Environmental statistics/hw.csv")
+x=sample(1:100,20)
+x
+SRS=data[x,2]
+SRS
+sd.srs=sd(SRS)
+n=20
+N=100
+se.srs=(sd.srs/n^0.5)*(1-n/N)^0.5
+se.srs
+mean.SRS=mean(SRS)
+est.tol=N*mean.SRS
+est.tol
+
---
+++ stratified random sample.R
@@ -0,0 +1,27 @@
+data=read.csv("C:/Users/Phoebe/Desktop/Environmental statistics/hw.csv")
+N=100
+x=sample(1:100,20)
+x
+stem(x,scale=2)
+x.1=c(2,5,32)
+x.2=c(8,18,28,38)
+x.3=c(51,52,53,56,66,73,75)
+x.4=c(48,57,60,80,90,98)
+n=c(3,4,7,6)
+StRS.1=data[x.1,2]
+StRS.2=data[x.2,2]
+StRS.3=data[x.3,2]
+StRS.4=data[x.4,2]
+sd.strs1=sd(StRS.1)
+sd.strs2=sd(StRS.2)
+sd.strs3=sd(StRS.3)
+sd.strs4=sd(StRS.4)
+sd.strsi=c(sd.strs1,sd.strs2,sd.strs3,sd.strs4)
+M=c(24,16,36,24)
+se.strsi=(sd.strsi/n^0.5)*(1-(n/M))^0.5
+se.strs=(sum((M/N)^2*se.strsi^2))^0.5
+se.strs
+mean.strsi=c(mean(StRS.1),mean(StRS.2),mean(StRS.3),mean(StRS.4))
+mean.strs=sum(mean.strsi*M/N)
+est.tol.strs=round(N*mean.strs,0)
+est.tol.strs
